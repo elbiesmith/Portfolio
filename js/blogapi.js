471a087ce031abae182ac5a94af0ca7c4979b63a
elbiesmith <elbie.smith@gmail.com>
2021-12-15 14:10:31 -0600

    added blog posts via api

---
+++ js/blogapi.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('//titan-mvcblog.herokuapp.com/GetTopXPosts/3')
+        .then(response => response.json())
+        .then(function (data) {
+            RenderApiData("blog1", data[0]);
+            RenderApiData("blog2", data[1]);
+            RenderApiData("blog3", data[2]);
+        });
+
+    function RenderApiData(id, data) {
+        //Establish the link out to the BlogPost Details...
+        let blogLink = `//titan-mvcblog.herokuapp.com/BlogPost/UrlFriendly/${data.slug}`;
+
+        //Work with the text anchor
+        //let textAnchor = $(`#${id} > div.card-body > h3 > a`);     
+        let textAnchor = document.querySelector(`#${id} > div.card-body > h3 > a`);
+        textAnchor.text = data.title;
+        textAnchor.setAttribute("href", blogLink);
+
+        //Work with the image anchor
+        let imageAnchor = document.querySelector(`#${id} > div.card-img > a`);
+        imageAnchor.setAttribute("href", blogLink);
+
+        //Work with the image
+        let image = document.querySelector(`#${id} > div.card-img > a > img`);
+        let imageData = `data:${data.contentType};base64,${data.imageData}`;
+        image.setAttribute("src", imageData);
+
+        //Work with the Abstract
+        let abstract = document.querySelector(`#${id} > div.card-body > p.card-description`);
+        abstract.innerHTML = data.abstract;
+
+        //Work with the created date
+        let options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        };
+        let created = document.querySelector(`#${id} > div.card-footer > div.post-date > span`);
+        let localDate = new Date(data.created).toLocaleDateString("en-US", options);
+        created.innerHTML = `${localDate}`;
+    }
+});
